47173885e67624313872ffa3298ea7563f65384e
Ty Tuff, Ph.D <ty.tuff@mail.mcgill.ca>
2021-06-02 00:36:56 -0400

    more installs

--- install.R
+++ install.R
@@ -2,7 +2,9 @@ install.packages(c("tidyverse",
                    "knitr", "rmarkdown",
                    "spocc", "mapr", "sf",
                    "scrubr", "osmdata", "raster",
-                   "osmdata", "mapr" 
+                   "osmdata", "mapr" , "maptools",
+                   "geojsonsf","rgdal", "taxize",
+                   "data.table", "mapview", "INLA"
                    
                    ))
 
